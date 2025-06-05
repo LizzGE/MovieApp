b46021c553615f2f88a7a412a9c6337a7ca7514e
Lizzet Garcia Estrada <lizzetgarcia180@gmail.com>
2025-06-05 16:10:21 -0600

    feat: added UI structure for upcoming, popular and top rated movies lists (part 3/3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+
+        viewBinding = true
+
+    }
+
 }
 
 dependencies {
--- app/src/main/java/com/cursosant/movieapp/MovieFragment.kt
+++ app/src/main/java/com/cursosant/movieapp/MovieFragment.kt
@@ -1,11 +1,25 @@
 package com.cursosant.movieapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
+import com.cursosant.movieapp.databinding.FragmentMovieBinding
 
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
+
+    private lateinit var binding : FragmentMovieBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMovieBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
