fe3b6dea53d7847074035c52464df4ffdd9a18e8
Lizzet Garcia Estrada <lizzetgarcia180@gmail.com>
2025-06-11 06:50:58 -0600

    feat(data/remote): add stub methods to MovieDataSource (2/2)

--- app/src/main/java/com/cursosant/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/cursosant/movieapp/data/remote/MovieDataSource.kt
@@ -1,10 +1,18 @@
 package com.cursosant.movieapp.data.remote
 
-/**
- * Proyecto: MovieApp
- * Creado por: lizze
- * Fecha: mar
- */
+import com.cursosant.movieapp.data.model.MovieList
 
 class MovieDataSource {
+
+    fun getUpcomingMovies() : MovieList {
+        return MovieList()
+    }
+
+    fun getTopRatedMovies() : MovieList {
+        return MovieList()
+    }
+
+    fun getPopularMovies() : MovieList {
+        return MovieList()
+    }
 }
