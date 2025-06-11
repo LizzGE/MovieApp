1cc38daf63595cb3e68cc7978def9577f3959baf
Lizzet Garcia Estrada <lizzetgarcia180@gmail.com>
2025-06-10 21:02:47 -0600

    feat(data): add initial data classes (1/2)

--- app/src/main/java/com/cursosant/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/cursosant/movieapp/data/model/Movie.kt
@@ -1,10 +1,21 @@
 package com.cursosant.movieapp.data.model
 
-/**
- * Proyecto: MovieApp
- * Creado por: lizze
- * Fecha: mar
- */
+data class Movie(
+    val id: Int = -1,
+    val adult: Boolean = false,
+    val genre_ids: List<Int> = listOf(),
+    val backdrop_path: String = "",
+    val orginal_title: String = "",
+    val original_language: String = "",
+    val overview: String = "",
+    val popularity: Double = -1.0,
+    val poster_path: String = "",
+    val release_date: String = "",
+    val title: String = "",
+    val video: Boolean = false,
+    val vote_average: Double = -1.0,
+    val vote_count: Int = -1
+)
 
-data class Movie(val id: Int = -1) {
-}
+
+data class MovieList(val results: List<Movie> = listOf())
